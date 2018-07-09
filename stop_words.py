32091f394c945153fe8875a504cccf41141f7a6a
varun26khan <40461632+varun26khan@users.noreply.github.com>
2018-07-09 16:25:08 +0530

    Delete stop_words.py

--- stop_words.py
+++
@@ -1,8 +0,0 @@
-scrapper = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'your', 'yours', 'yourself', 'a', 'an', 'the',
-            'and', 'at', 'by', 'against', 'between', 'before', 'after', 'above', 'below', 'to', 'from', 'of', 'down',
-            'in', 'out', 'on', 'off', 'over', 'under', 'only', 'very', 'now', 'classes', 'home', 'tutors', 'company',
-            'tutor', 'tutorials', 'academy', 'classes', 'pvt', 'ltd', 'institute', 'exams', 'education', 'coaching',
-            'tutorial', 'point', 'center', 'centre', 'opp', 'near', 'behind', 'square', 'opposite', 'above', 'below',
-            'eduventures', 'career']
-
-
